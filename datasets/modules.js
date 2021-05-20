cc9717b64d17c6be9dcd4541525e6fb6102d76e9
XeonLyfe <xeonlyfeyt@gmail.com>
2021-05-20 17:40:55 -0400

    lo
    lol

---
+++ datasets/modules.js
@@ -0,0 +1,51 @@
+var dictionaryModules = {
+    "Combat" : {
+        "Anti Crystal": "Blocks crystal damage with pressure plates.",
+        "Anti Hole Fill": "Puts buttons in holes.",
+        "Auto Anvil" : "Automatically places anvils on an enemies head to break there helmet. (Broken)",
+        "Auto Crystal" : "Automatically places and breaks crystals",
+        "Auto Offhand" : "Automatically moves an item to your offhand",
+        "Auto Water" : "Automatically places a water bucket at your feet. (Broken atm but ik a fix)",
+        "Hole Filler" : "Fills da holes",
+        "Aura" : "Autoaticamlly hits entitys around you."
+    },
+    "Player" : {
+        "Velocity": "Reduces the amount of knockback you take. (Broken)",
+        "X Carry": "Gives you 4 extra inventory slots."
+    },
+    "Movement" : {
+        "Ledges" : "Prevents you from falling off of blocks.",
+        "Long jump" : "Makes you jump really far.",
+        "ReverseStep" : "Makes you fall fast coming off of blocks.",
+        "Step" : "Allows you to step up 1 or 2 blocks.",
+        "Yaw Lock" : "Locks your yaw so you only go in one direction."
+    },
+    "Render" : {
+        "BlockHighlight" : "A better way to highlight the block you are looking at.",
+        "Chat Modifications" : "Modifies the look of the chat.",
+        "Fog Color" : "Allows you to change the color of fog and sky (Causes weird chunk rendering like only 4 chunks load :/)",
+        "Full Bright" : "Makes it look like its always daytime.",
+        "Hole ESP" : "Renders the safe holes.",
+        "Viewmodel" : "The viewmodel changer (Causes weird chunk rendering like only 4 chunks load).",
+        "Zoom" : "A Module like optifine zoom but better."
+    },
+    "Misc" : {
+        "Chat Logger" : "Logs chat into a file.",
+        "Chat Suffix" : "Lets everyone know about your based client.",
+        "DeathCoords" : "Creates a DeathCoords list in chat.",
+        "Fake Player" : "Creates a fake player for configging.",
+        "Fast Util" : "Allows you to use your utils really fast."
+    },
+    "Client" : {
+        "RainbowModule" : "The module that handles the rainbow.",
+        "ClickGuiModule" : "The Click GUI module",
+        "Hud" : "Renders hud componenents on screen.",
+        "RPC" : "Does the rpc thingie.",
+        "ConsoleModule" : "Displays a console.",
+        "TestClickGUI" : "nonworking.",
+        "DesktopNotifs" : "sends Notifies to decktop.",
+        "Credits" : "Shows the clients Credits.",
+        "Capes" : "Renders capes on users (WIP)"
+    },
+
+}
---
+++ datasets/playerDev.js
@@ -0,0 +1,7 @@
+var playerDev = {
+    "000XeonLyfe" : "Owner/Main Dev",
+    "001Hollow" : "Developer",
+    "002IronException" : "Developer",
+    "003VacCat" : "Developer",
+    "004_bt" : "Developer"
+}
